6be2d409ae3d32319314a3b6b52947e49a86137d
Tong1233 <74699244+Tong1233@users.noreply.github.com>
2023-11-10 14:12:42 -0500

    server.js cors change

--- sentiment-analysis-backend/server.js
+++ sentiment-analysis-backend/server.js
@@ -6,13 +6,10 @@ const cors = require('cors'); // Import the cors package
 const tokenizer = new natural.WordTokenizer();
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
-const corsOptions = {
-    origin: 'http://localhost:5173',
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Define a route handler for the root URL
